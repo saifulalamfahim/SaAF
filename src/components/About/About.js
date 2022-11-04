4e180ed6e054a4677fe001287e9e19756c55fa99
saifulalamfahime <you@example.com>
2022-11-04 21:46:41 +0600

    About section responsiveness completed for 500px

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
                 
                 
                 
-                fter that i’m curios about “<span className='highlight-text hudai'>Frontend Designing and building user interface</span> ”So as much as <br /> possible i started to learn coding. First of all, I learned “ <span className='highlight-text'>HTML & CSS</span> ”and some <span className='highlight-text'>CSS framework like <br /> Bootstrap, tailwind CSS</span> etc. then I realized these too;s are not enough for “ <span className='hudai'>frontend development</span> ”So I started <br /> my journey with <span className='highlight-text hudai'>javaScript and some JS framework like ReactJS, TypeScript</span> etc, Since last <br /> 2 years I built some beutifull local and my own website project for practice. (Click Heare to see myproject) <br /> 
+                after that i’m curios about “<span className='highlight-text hudai'>Frontend Designing and building user interface</span> ”So as much as <br /> possible i started to learn coding. First of all, I learned “ <span className='highlight-text'>HTML & CSS</span> ”and some <span className='highlight-text'>CSS framework like <br /> Bootstrap, tailwind CSS</span> etc. then I realized these too;s are not enough for “ <span className='hudai'>frontend development</span> ”So I started <br /> my journey with <span className='highlight-text hudai'>javaScript and some JS framework like ReactJS, TypeScript</span> etc, Since last <br /> 2 years I built some beutifull local and my own website project for practice. (Click Heare to see myproject) <br /> 
                 Now, Finally I’m ready to start my journey with you. <br />
                 Trust me, I’ll do best things to you!</p>
                 <p>Thanks</p>
