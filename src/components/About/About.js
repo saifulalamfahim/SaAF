2aa3c66266d4f4a21e7ecddcbef6429bd0c59950
saifulalamfahime <you@example.com>
2023-03-22 23:57:05 +0600

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,19 +4,30 @@ import './About.css';
 const About = () => {
     return (
         <div className='about-container'>
-            <h1>Something about my journey</h1>
+            <h1>This is the story of my journey.</h1>
             <div className='About-details'>
-                <p>Since beginning my journey as a presentation designer using “Office Powerpoint” over 3 years ago. then I learned other <br /> designing tools like<span className='highlight-text'>Adobe Photoshop, Adobe XD, Figma,</span> etc. And done some beautiful projects using these <br /> tools. <br />
-                
-                
-                
-                after that, I’m curious about “<span className='highlight-text hudai'>Frontend Designing and building user interface</span> ”So as much as <br /> possible I started to learn to code. First of all, I learned “ <span className='highlight-text'>HTML & CSS</span> ”and some <span className='highlight-text'>CSS frameworks like <br /> Bootstrap, tailwind CSS,</span> etc. then I realized these tools are not enough for “ <span className='hudai'>frontend development</span> ”So I started <br /> my journey with <span className='highlight-text hudai'>JavaScript and some JS frameworks like React, TypeScript,</span> etc, Since last <br />2 years I built some beautiful local and my website project for practice. (Scroll below to see my project) <br /> 
-                Now, Finally, I’m ready to start my journey with you.<br />
-                Trust me, I’ll do the best things for you!</p>
-                <p>Thanks</p>
+                <p>Once upon a time, three years ago, I set out on a journey of design. Armed with only my trusty <span className='highlight-text'>Office Powerpoint</span>, I started creating   <br /> presentations that dazzled and impressed. But I knew I could do more. So, I delved deeper and began learning other designing tools,  such as <br /> <span className='highlight-text'> Adobe Photoshop, Adobe XD, and Figma.</span> With these powerful tools in hand, I created some of the most stunning projects <br />  you could ever imagine. <br /> <br />
+
+                However, my thirst for knowledge was not yet quenched. I yearned to explore the realm of“<span className='highlight-text hudai'>Frontend Design and building <br /> user interfaces</span>”. And so, I made the bold decision to learn how to code. Starting with the basics of“ <span className='highlight-text'>HTML & CSS</span>” <br /> and some <span className='highlight-text'>CSS frameworks like Bootstrap, Tailwind CSS,</span> I soon realized that these tools were insufficient for my ambitions.  <br />  So, I took the plunge and began my journey with <span className='highlight-text hudai'>JavaScript and JS frameworks like React, TypeScript,</span> etc, <br />
+                <br />Fast forward two years, and I had become a master of my craft. I had built beautiful websites for my local businesses and my practice, <br /> each one better than the last. (Please scroll down to see my project)<br /> <br />
+                And now, I stand before you, ready to embark on this new journey with you.<br />
+                Trust me, I have the skills and the passion to create something truly amazing. Let's build something incredible together.</p>
+                {/* <p>Thanks</p> */}
             </div>
         </div>
     );
 };
 
 export default About;
+
+
+
+// The journey began over 3 years ago when I began designing presentations using Office Powerpoint. My next step was to learn other designing tools such as Adobe Photoshop, Adobe XD, and Figma. These tools have also been used to create some beautiful projects.
+
+// Afterward, I was curious about "Frontend Design and building user interfaces", so I decided to learn how to code. First of all, I discovered “ HTML & CSS ”and some CSS frameworks like Bootstrap, Tailwind CSS, etc. Then I realized these tools were inadequate for " frontend development ", so I began my journey with JavaScript and JS frameworks like React, TypeScript, etc. 
+
+// In the last two years, I have built some beautiful websites for my local business and my practice. (Please scroll down to see my project)
+// Here I am, finally, ready to embark on this journey with you.
+// Trust me, I’ll do what's best for you!
+
+// Thanks
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,14 +9,14 @@ const Header = () => {
             <a href="https://www.linkedin.com/in/saafahim" target="_blank" rel="noreferrer"><Icon className='icon' icon="bi:linkedin" /></a>
             <a href="https://www.facebook.com/fahim.srlsy" target="_blank" rel="noreferrer"> <Icon className='icon' icon="brandico:facebook-rect" /></a>
            <a href="https://github.com/saifulalamfahim" target="_blank" rel="noreferrer"> <Icon className='icon1' icon="fa-brands:github-square" /></a>
-           <a href="https://www.instagram.com/fahim_vha" target="_blank" rel="noreferrer"><Icon className='icon1' icon="fa-brands:instagram-square" /></a>
+           <a href="https://www.instagram.com/emptycoding/" target="_blank" rel="noreferrer"><Icon className='icon1' icon="fa-brands:instagram-square" /></a>
             </div>
             <div className='saaf'>
                 <h1>SaAF</h1>
                 <p>is here for you</p>
             </div>
             <div className='saaf-details'>
-                <p>Hey! I’m Saiful Alam, you can call me Fahim. I’m a<br /> self-made “ <span>Web developer & Designer</span> ”</p>
+                <p>Hello there, I'm Saiful Alam, but you can call me Fahim. I consider <br /> myself a self-taught“<span>Front-end developer & Designer</span>” <br /> who enjoys storytelling.</p>
             </div>
             <a href="https://drive.google.com/file/d/1VIggTPyHy-b6M_-1qhDq_tVyulsuKnye/view" target="_blank"  rel="noreferrer"><button>My Resume</button></a>
             <p></p>
